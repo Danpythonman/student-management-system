fe2045bbd1eacabd6a77e0cff9cdb28a43c19c65
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-06-01 19:05:56 -0400

    Added more users to the initial database.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,6 @@
 INSERT INTO student (id, name, email, dob) VALUES (DEFAULT, 'Dan', 'dan@gmail.com', '2000-01-01');
+INSERT INTO student (id, name, email, dob) VALUES (DEFAULT, 'Joe', 'joe@gmail.com', '2005-06-12');
+INSERT INTO student (id, name, email, dob) VALUES (DEFAULT, 'Don', 'don@gmail.com', '2009-10-11');
+INSERT INTO student (id, name, email, dob) VALUES (DEFAULT, 'Dom', 'dom@gmail.com', '2010-02-21');
+INSERT INTO student (id, name, email, dob) VALUES (DEFAULT, 'Sam', 'sam@gmail.com', '2012-07-18');
+INSERT INTO student (id, name, email, dob) VALUES (DEFAULT, 'May', 'may@gmail.com', '1992-02-05');
