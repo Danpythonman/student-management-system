32cb1fe8b96d0c42494197ff594099cf7c95ea65
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-05-31 21:48:22 -0400

    Added controller for rendering thymeleaf templates.

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,11 +1,17 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping(path = "/api")
 public class StudentController {
 
@@ -16,16 +22,19 @@ public class StudentController {
         this.studentService = studentService;
     }
 
+    @ResponseBody
     @GetMapping("/students")
     public List<Student> getStudents() {
         return this.studentService.getStudents();
     }
 
+    @ResponseBody
     @GetMapping("/student/{id}")
     public Student getStudentById(@PathVariable("id") Long id) {
         return this.studentService.getStudentById(id);
     }
 
+    @ResponseBody
     @PostMapping("/student")
     public void addStudent(@RequestBody Student student) {
         this.studentService.saveStudent(student);
@@ -42,6 +51,30 @@ public class StudentController {
     @DeleteMapping("/student/{id}")
     public void deleteStudent(@PathVariable("id") Long id) {
         this.studentService.deleteStudentById(id);
+    }
+
+    @GetMapping("/student/{id}/email")
+    public ResponseEntity<String> getStudentEmail(@PathVariable("id") Long id) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("custom-header", "bar");
+        return new ResponseEntity<String>(
+                this.studentService.getStudentById(id).getEmail(),
+                headers,
+                HttpStatus.ACCEPTED
+        );
+    }
+
+    @GetMapping("/student/{id}/dob")
+    public ResponseEntity<LocalDate> getStudentDob(@PathVariable("id") Long id) {
+        return ResponseEntity.status(401)
+                .header("custom-header", "foo")
+                .body(this.studentService.getStudentById(id).getDob());
+    }
+
+    @GetMapping("/student/page")
+    public String getStudentPage2(Model model) {
+        model.addAttribute("attribute", "Text from controller");
+        return "student.html";
     }
 
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,6 +1,8 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
