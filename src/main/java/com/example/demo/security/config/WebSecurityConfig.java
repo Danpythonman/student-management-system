6ca194faa8e17ef864ef0a75a0951cbea0cf8d65
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-06-21 11:55:39 -0400

    Moved security config to separate package, also added password encoder and more configuration

---
+++ src/main/java/com/example/demo/security/config/WebSecurityConfig.java
@@ -0,0 +1,57 @@
+package com.example.demo.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.example.demo.user.UserService;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public WebSecurityConfig(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userService = userService;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity security) throws Exception
+    {
+        security
+        .csrf().disable()
+        .authorizeRequests()
+        .antMatchers("/signIn").permitAll()
+        .antMatchers("/register").permitAll()
+        .antMatchers("/api/register").permitAll()
+        .anyRequest().authenticated()
+        .and().formLogin().loginPage("/signIn").usernameParameter("email").permitAll();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(this.daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setPasswordEncoder(this.bCryptPasswordEncoder);
+
+        provider.setUserDetailsService(this.userService);
+
+        return provider;
+    }
+
+}
