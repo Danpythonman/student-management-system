cfb44a3049cb987d306dc5fe3aa2ac699cbc8388
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-06-24 12:28:42 -0400

    Added user repository query to get all chatrooms for a specific user. Had to make a new interface for chatrooms because using the chatroom class resulted in errors.

---
+++ src/main/java/com/example/demo/chatroom/ChatroomInterface.java
@@ -0,0 +1,7 @@
+package com.example.demo.chatroom;
+
+public interface ChatroomInterface {
+    public Long getId();
+    public String getName();
+    public Long getOwnerId();
+}
--- src/main/java/com/example/demo/user/UserRepository.java
+++ src/main/java/com/example/demo/user/UserRepository.java
@@ -1,11 +1,25 @@
 package com.example.demo.user;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.demo.chatroom.ChatroomInterface;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
+
+    @Query(
+        value = "SELECT chatroom.id AS id, chatroom.name AS name, chatroom.owner_id as owner_id FROM chatroom"
+            + " INNER JOIN user_chatroom"
+            + " ON chatroom.id = user_chatroom.chatroom_id"
+            + " WHERE user_chatroom.user_id = :id",
+        nativeQuery = true
+    )
+    List<ChatroomInterface> findChatroomsForUser(@Param("id") Long id);
 
 }
