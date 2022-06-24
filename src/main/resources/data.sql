ac3810e2059792b47e67c9117a4089ac8b9f0145
Daniel Di Giovanni <dannyjdigio@gmail.com>
2022-06-24 12:23:48 -0400

    Inserted a chatroom, user_chatroom and chat into database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,11 @@ INSERT INTO student (id, name, email, dob) VALUES (DEFAULT, 'May', 'may@gmail.co
 INSERT INTO student (id, name, email, dob) VALUES (DEFAULT, 'BenGerman Thong', 'ben@gmail.com', '2002-04-20');
 
 INSERT INTO users (id, fname, lname, email, password, locked, enabled) VALUES (DEFAULT, 'Bengo', '3022', 'bengo3022@gmail.com', '$2a$10$iH2k64N5T/OvKm0gfMaQnu.Hnnj/UrV7qa.UBXg4LsuzYc7FwHVya', 'false', 'true');
+
+INSERT INTO event (id, name, startDate, endDate) VALUES (DEFAULT, 'Ben Bornday', '2002-05-20 13:00:00', '2002-05-20 14:00:00');
+
+INSERT INTO chatroom (id, name, owner_id) VALUES (DEFAULT, 'Chat of Ben', 1);
+
+INSERT INTO user_chatroom (id, user_id, chatroom_id) VALUES (DEFAULT, 1, 1);
+
+INSERT INTO CHAT (id, sent_at, message, chatroom_id, sender_id) VALUES (DEFAULT, '2022-06-23 21:00:00', 'test message', 1, 1);
